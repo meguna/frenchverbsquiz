cbb5afb77bd2168d05816cdf7ff8c907a67f6b9a
Meg <megunaokawa@gmail.com>
2019-11-11 07:57:50 -0500

    Updates

---
+++ precache-manifest.e735ec6e992d6d640ecf8378740ad4e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57623030fef65f4c5a8bb5c76e71e78c",
+    "url": "/frenchverbsquiz/index.html"
+  },
+  {
+    "revision": "847f52d36083107d27f1",
+    "url": "/frenchverbsquiz/static/css/main.8c67dbd5.chunk.css"
+  },
+  {
+    "revision": "78e20a352db2332280c6",
+    "url": "/frenchverbsquiz/static/js/2.c3f3d1ad.chunk.js"
+  },
+  {
+    "revision": "847f52d36083107d27f1",
+    "url": "/frenchverbsquiz/static/js/main.1b3c112c.chunk.js"
+  },
+  {
+    "revision": "403c0ae2ebd3b442200e",
+    "url": "/frenchverbsquiz/static/js/runtime-main.52529e01.js"
+  }
+]);
